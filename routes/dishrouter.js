c4678f216201d0ea5415f2929a5cd8871a99975c
Randkill <randkill.randkill@gmail.com>
2019-07-12 00:12:21 +0430

    now handling /:dishId endpoints for /dishes

--- routes/dishrouter.js
+++ routes/dishrouter.js
@@ -25,4 +25,27 @@ dishRouter.route('/').all((req,res,next) => {      //app.all() : no matter which
     res.end('Deleting all the dishes!');
 });
 
+//now handling /dishes/:dishId
+
+dishRouter.route('/:dishId').all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    next();
+}).get((req, res, next) => {
+    res.end('Will send details of the dish : ' +
+    req.params.dishId + ' to you!');
+}).post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Post operation not supported on /dishes/' +
+    req.params.dishId +'!');
+}).post((req, res, next) => {
+    res.write('Updating the dish : ' + req.params.dishId + '\n');      //add a line to reply messege
+    res.end('Will update the dish : ' +
+    req.body.name + ' with details : ' +
+    req.body.description);
+}).delete((req, res, next) => {
+    res.end('Deleting dish : ' +
+    req.params.dishId);
+});
+
 module.exports = dishRouter;
